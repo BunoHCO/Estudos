dde16a7fd2a434a9710c4af398b874efaf361cc4
bunooh <olivierabruno2011@gmail.com>
2024-07-04 17:13:57 -0300

    Desafio

---
+++ desafio7.py
@@ -0,0 +1,3 @@
+nota1=float(input("Primeira nota do aluno: "))
+nota2=float(input("Segunda nota do aluno: "))
+print("A média entre {:.1f} e {:.1f} é igual a {:.1f}".format(nota1, nota2, ((nota1+nota2)/2)))
