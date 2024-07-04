0d2de705a0bab3b062eb1567ffb7573bb7904305
bunooh <olivierabruno2011@gmail.com>
2024-07-04 17:00:17 -0300

    Desafio

---
+++ desafio6.py
@@ -0,0 +1,4 @@
+num=int(input("Digite um numero: "))
+print("O Dobro de {} é {} ".format(num, (num+num)))
+print("O Triplo de {} é {}".format(num, (num*3)))
+print("A raiz quadrada de {} é {:.2f}".format(num, (num**(1/2))))
