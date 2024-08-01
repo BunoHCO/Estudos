b163a1f09b0d26af0a02d29f0e6e4eaf40e1af07
bunooh <olivierabruno2011@gmail.com>
2024-08-01 17:59:38 -0300

    desafio

---
+++ desafio31.py
@@ -0,0 +1,8 @@
+km = int(input("Digite a distancia da viagem em KM: "))
+
+if km <= 200:
+    preço = km * 0.5
+    print("O preço que dará por uma viagem de {}KM será RS${:.2f}!".format(km, preço))
+else:
+    preço = km *0.45
+    print("O preço que dara por uma viagem de {}KM será R${:.2f}".format(km ,preço))
