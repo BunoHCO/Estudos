e57fdb406d1ee3e788250cf3f62b0f56a11f6ce2
bunooh <olivierabruno2011@gmail.com>
2024-08-05 16:03:53 -0300

    desafios

--- desafio31.py
+++ desafio31.py
@@ -4,5 +4,5 @@ if km <= 200:
     preço = km * 0.5
     print("O preço que dará por uma viagem de {}KM será RS${:.2f}!".format(km, preço))
 else:
-    preço = km *0.45
+    preço = km * 0.45
     print("O preço que dara por uma viagem de {}KM será R${:.2f}".format(km ,preço))
